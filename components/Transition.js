5470e4e012ccd6e5cb6f502e0ed7357edc09b48b
kumanboy <husandavronov13@gmail.com>
2024-05-10 11:42:44 +0500

    Transition.js end

--- components/Transition.js
+++ components/Transition.js
@@ -1,9 +1,40 @@
-const Transition = () => {
-  return (
-    <>
-      <div>Transition</div>
-    </>
-  );
-};
+import {motion} from 'framer-motion'
 
+const transitionVariants = {
+    initial: {
+        x: '100%',
+        width: '100%',
+
+    },
+    animate: {
+        x: '0',
+        width: '100%',
+    },
+    exit: {
+        x: ['0%', '100%'],
+        width: ['0%', '100%'],
+    }
+}
+
+const Transition = () => {
+    return (
+        <>
+            <motion.div className={'fixed top-0 bottom-0 right-full z-30 bg-[#2e2257]'} variants={transitionVariants}
+                        initial={"initial"} animate={'animate'} exit={"exit"}
+                        transition={{delay: 0.2, duration: 0.6, ease: "easeInOut"}}>
+            </motion.div>
+            <motion.div className={'fixed top-0 bottom-0 right-full z-20 bg-[#3b2d71]'} variants={transitionVariants}
+                        initial={"initial"} animate={'animate'} exit={"exit"}
+                        transition={{delay: 0.4, duration: 0.6, ease: "easeInOut"}}>
+            </motion.div>
+            <motion.div className={'fixed top-0 bottom-0 right-full z-10 bg-[#4b3792]'} variants={transitionVariants}
+                        initial={"initial"} animate={'animate'} exit={"exit"}
+                        transition={{delay: 0.6, duration: 0.6, ease: "easeInOut"}}>
+            </motion.div>
+        </>
+    );
+};
 export default Transition;
+
+// TODO:`29:12 qator xato bolishi mn`
+
